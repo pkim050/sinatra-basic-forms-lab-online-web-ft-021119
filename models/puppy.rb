1f381dc04025adcc046e88d373dfadd95b56e6b5
Patrick Kim <pkim050@gmail.com>
2019-04-05 04:39:16 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,7 +1,7 @@
 class Puppy
   attr_accessor :name, :breed, :months_old
 
-  def initialize(name, breed, months_old)
+  def initialize(name = "", breed = "", months_old = 0)
     @name = name
     @breed = breed
     @months_old = months_old
