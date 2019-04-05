b9725d384a960986a98a50972c467aea3196d0ea
Patrick Kim <pkim050@gmail.com>
2019-04-05 05:21:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,10 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/' do
-    erb :index.erb
+    erb :index
   end
 
   get '/new' do
-    erb :create_puppy.erg
+    erb :create_puppy
   end
+
+  post '/puppy' do
+    erb :display_puppy
 end
