d9f3621b8d47e30bb9ba9f53b260aa975e3c24b9
Patrick Kim <pkim050@gmail.com>
2019-04-05 05:47:22 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,7 +1,7 @@
 class Puppy
   attr_accessor :name, :breed, :age
 
-  def initialize(name = "", breed = "", age = 0)
+  def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
